2f25dba751c67ac0b267c0416573621baacd3f43
Meowmir <disewji@gmail.com>
2024-01-10 20:16:23 +0100

    boiler

---
+++ main.js
@@ -0,0 +1,31 @@
+// Main Process
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+  // Browser Window <- Renderer Process
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    backgroundColor: "red",
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  win.loadFile("index.html");
+  win.webContents.openDevTools();
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
