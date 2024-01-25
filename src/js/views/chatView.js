d15668d30598fcdb066111046a7cc4ba207296b0
Meowmir <disewji@gmail.com>
2024-01-25 12:35:14 +0100

    add css to move button and title to center of div, looks cleaner, also remove joined chats from available chats in chatview

--- src/js/views/chatView.js
+++ src/js/views/chatView.js
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
+
 import { withBaseLayout } from "../../layout/base";
 import ChatUsersList from "../components/chatUsersList";
 import ChatMessagesList from "../components/chatMessagesList";
 import ViewTitle from "../components/shared/viewTitle";
 
+import { subscribeToChat } from "../actions/chats";
+
 
 function ChatView() {
   const { id } = useParams()
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const unsubFromChat = dispatch(subscribeToChat(id))
+    return () => {
+      unsubFromChat()
+    }
+  }, []);
 
    return (
        <div className="row no-gutters fh">
